d23180b9f0eb7c2fa34b37cfde0936dc986fc335
BT_1N3_02 <sample@bths.edu>
2024-03-13 09:10:40 -0400

    commit

--- src/Logic.java
+++ src/Logic.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 public class Logic {
+    private Spaces[][] connectFourBoard;
+    private Scanner myScanner;
     public Logic() {
-
+        myScanner = new Scanner(System.in);
     }
     public void start() {
         welcomeUser();
+    }
+
+    public void createEmptyBoard() {
+        connectFourBoard = new Spaces[6][7];
+        for(int i = 0; i < connectFourBoard.length; i++) {
+            for(int j = 0; j < connectFourBoard[0].length; j++) {
+
+            }
+        }
     }
     public void welcomeUser() {
         System.out.println("Welcome to Connect 4!");
