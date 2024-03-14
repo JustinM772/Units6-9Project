75cdfa7034889f30a53d678b02f06a1f4f3d4dbb
BT_1N3_02 <sample@bths.edu>
2024-03-14 09:08:41 -0400

    commit

--- src/Red.java
+++ src/Red.java
@@ -2,7 +2,7 @@ public class Red extends Spaces {
     public Red(int row, int col) {
         super(row, col);
     }
-
+    @Override
     public String getSymbol() {
         return  "⬜";
     }
--- src/Yellow.java
+++ src/Yellow.java
@@ -2,7 +2,7 @@ public class Yellow extends Spaces{
     public Yellow(int row, int col) {
         super(row, col);
     }
-
+    @Override
     public String getSymbol() {
         return  "⬜";
     }
